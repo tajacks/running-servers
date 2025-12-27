1b9c911d2f8025e681baa4d6d0035f1c6152caa2
Thomas Jack <tom@tomjack.ca>
2025-12-26 22:00:17 -0500

    Cleanup

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,13 +9,7 @@ export default function(eleventyConfig) {
   // Markdown renderer for shortcode content
   const md = markdownIt({ html: true });
 
-  // Simple shortcode: {% hello "world" %} outputs "world hello"
-  eleventyConfig.addShortcode("hello", function(text) {
-    return `${text} hello`;
-  });
-
-
-  // SHORT CODES //
+  // COMMAND SHORT CODES //
   const copyIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>`;
   const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>`;
 
